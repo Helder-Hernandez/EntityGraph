4ba679e2eb3a3639eeb52ea3de7f2ed08c6abcbe
Helder-Hernandez <josue10hd@gmail.com>
2019-08-12 11:08:40 -0600

    Confirmando cambios en carpeta bin

---
+++ bin/src/main/resources/db/migration/h2/V8__insert_office.sql
@@ -0,0 +1,4 @@
+insert into office(id, name, address_id, department_id) VALUES (1, 'Office of S&M Boston', 4, 1);
+insert into office(id, name, address_id, department_id) VALUES (2, 'Office of S&M New York', 5, 1);
+insert into office(id, name, address_id, department_id) VALUES (3, 'Office of R&D Boston', 6, 2);
+insert into office(id, name, address_id, department_id) VALUES (4, 'Office of A Los Angeles', 7, 3);
